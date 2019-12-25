86d2167404be81b052889f30c4440cff489d9169
Wzp-2008 <55318422+Wzp-2008@users.noreply.github.com>
2019-12-25 09:30:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import Tkinter
+import tkinter as Tkinter
 import pypinyin
+
 top = Tkinter.Tk()
+input = Tkinter.Entry(top)
 def jiami():
-  
-jm = Tkinter.Button(top,text="加密",command=
+  print(pypinyin.pinyin(input.get()))
+  input.select_clear()
+jm = Tkinter.Button(top,text="加密",command=jiami)
+jm.pack()
+input.pack()
 top.mainloop()
